bf9304ff752e91f7b1b94146383fde1459f36b73
Kasper Guldmann Nielsen <git@kalleguld.dk>
2014-11-14 11:50:23 +0100

    MVC Project started

---
+++ JohannesVidnerProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JohannesVidnerProject.MvcApplication" Language="C#" %>
---
+++ JohannesVidnerProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JohannesVidnerProject.Startup))]
+namespace JohannesVidnerProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
