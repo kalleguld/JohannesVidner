001fa21b99473050d32883a9d0e8c7a4d493ca42
kalleguld <kalleguld@gmail.com>
2014-11-17 13:21:44 +0100

    Added User class

---
+++ Model/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class User
+    {
+        private string _password;
+
+        public User(string username, string password)
+        {
+            Username = username;
+            _password = password;
+        }
+        public string Username { get; private set; }
+        public string Name { get; set; }
+        public string Password
+        {
+            set
+            {
+                _password = value;
+                PasswordLastChanged = new DateTime();
+            } 
+        }
+        public DateTime PasswordLastChanged { get; private set; }
+
+        public bool HasWriteAccess { get; set; }
+        public bool IsUserAdmin { get; set; }
+        
+
+
+        public bool IsPasswordCorrect(string password)
+        {
+            return _password != null && _password == password;
+        }
+
+
+
+    }
+}
