8de5f66bcc84b1a4233308f21d413a6561e52648
Anders <anders.ahp@gmail.com>
2014-11-18 11:59:19 +0100

    Login side næsten færdig Mangler at tjekke op mod bruger info som ikke er lavet endnu. Mangler også at tjekke ordentlig hvilken bruger der er logget ind

--- JohannesVidnerProject/Controllers/HomeController.cs
+++ JohannesVidnerProject/Controllers/HomeController.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using JohannesVidnerProject.Models;
+using Microsoft.AspNet.Identity.Owin;
+using Services;
 
 namespace JohannesVidnerProject.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
+            if (!Service.GetInstance().LoggedIn)
+            {
+                Service.GetInstance().LoggedIn = true;
+                return RedirectToRoute(new { controller = "Home", action = "Login" });   
+            }
             return View();
         }
 
@@ -25,6 +35,29 @@ namespace JohannesVidnerProject.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        
+
+        // Http GET
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your Login page.";
+
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            // lav tjek på om pw og email er ok
+            return RedirectToRoute(new { controller = "Home", action = "Index" });
         }
     }
 }
