785ba2edd5b9e68c372ce8445fa0a8f81aa2ca4a
kalleguld <kalleguld@gmail.com>
2014-11-18 12:23:46 +0100

    Added DbService

---
+++ Services/DbService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    class DbService
+    {
+        private static DbService _instance;
+
+        private DbService()
+        {
+            //TODO 
+        }
+        public static DbService Instance
+        {
+            get { return _instance ?? (_instance = new DbService()); }
+        }
+    }
+}
