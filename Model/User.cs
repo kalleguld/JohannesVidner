521f78aa082a7aef0788f47c37a2853d8a331b18
Holgeroth <holger_k_n@hotmail.com>
2014-11-18 11:56:59 +0100

    Tilføjet database udtræk, lavet klasser og ændret i klasserne

--- Model/User.cs
+++ Model/User.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace Model
 {
+
+
     class User
     {
         private string _password;
@@ -37,7 +39,90 @@ namespace Model
             return _password != null && _password == password;
         }
 
+        private Publication _publication ;
+        public Publication Publication
+        {
+            get { return _publication; }
+            set
+            {
+                _publication = value;
+                _publication.AddUser(this);
+            }
+        }
+    }
 
+    class Publication
+    {
+        private readonly int _id;
+        public int Id
+        {
+            get { return _id; }
+        }
 
+        private readonly string _name;
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        private readonly List<User> _users;
+
+        public List<User> Users
+        {
+            get { return _users; }
+        }
+
+        public void AddUser(User user)
+        {
+            _users.Add(user);
+        }
+
+        public void RemoveUser(User user)
+        {
+            _users.Remove(user);
+        }
+
+        private readonly List<Edition> _editions;
+
+        public List<Edition> Editions
+        {
+            get { return _editions; }
+        }
+
+        public void AddEdition(Edition edition)
+        {
+            _editions.Add(edition);
+        }
+
+        public void RemoveEdition(Edition edition)
+        {
+            _editions.Remove(edition);
+        }
+
+        public Publication(int id, string name)
+        {
+            _id = id;
+            _name = name;
+            _users = new List<User>();
+            _editions = new List<Edition>();
+        }
+    }
+
+    class Edition
+    {
+        public DateTime RunningStarted { get; set; }
+        public int MaxMissingPages { get; set; }
+        public bool Running { get; set; }
+        public string Log { get; set; }
+        public string Errormessage { get; set; }
+
+        private readonly Publication _publication;
+        public Publication Publication { get { return _publication; }}
+
+        public Edition(Publication publication)
+        {
+            _publication = publication;
+            _publication.AddEdition(this);
+        }
     }
 }
