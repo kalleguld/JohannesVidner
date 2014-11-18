cd68e91a2d2cbab37d5fd8e9e71edf554f9e3c55
kalleguld <kalleguld@gmail.com>
2014-11-18 11:43:26 +0100

    added Userlist view

---
+++ JohannesVidnerProject/Controllers/UserManagerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JohannesVidnerProject.Controllers
+{
+    public class UserManagerController : Controller
+    {
+        // GET: UserManager
+        public ActionResult Index()
+        {
+            return List();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
