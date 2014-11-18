21148bd4304f1d7e90b4e0563594f718696a5686
kalleguld <kalleguld@gmail.com>
2014-11-18 12:08:36 +0100

    added Session Service

---
+++ JohannesVidnerProject/SessionService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Model;
+
+namespace JohannesVidnerProject
+{
+    public class SessionService
+    {
+
+        public  User CurrentUser
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
--- Model/User.cs
+++ Model/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class User
+    public class User
     {
         private string _password;
 
