5b32fc23a3c670a231d8311998289fe74611ead7
Kasper Guldmann Nielsen <git@kalleguld.dk>
2014-11-19 10:07:39 +0100

    List created

---
+++ JohannesVidnerProject/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+using Services;
+
+namespace JohannesVidnerProject.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Index()
+        {
+            var sessionService = SessionService.Instance;
+            var currentUser = sessionService.CurrentUser;
+            if (currentUser == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            if (!currentUser.IsUserAdmin)
+            {
+                //TODO redirect to an error page instead of giving an empty list
+                return View(new List<User>());
+            }
+
+            var dbService = DbService.Instance;
+            var users = dbService.GetUsersByPublicationRecursive(currentUser.Publication);
+            return View(users);
+        }
+
+        public ActionResult Create()
+        {
+            var sessionService = SessionService.Instance;
+            var currentUser = sessionService.CurrentUser;
+            if (currentUser == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            if (!currentUser.IsUserAdmin)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
--- Services/DbService.cs
+++ Services/DbService.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace Services
 {
-    class DbService
+    public class DbService
     {
         private static DbService _instance;
 
@@ -18,5 +19,14 @@ namespace Services
         {
             get { return _instance ?? (_instance = new DbService()); }
         }
+
+        /// <summary>
+        /// Returns all users assigned to the publication or a sub-publication
+        /// </summary>
+        public ICollection<User> GetUsersByPublicationRecursive(Publication publication)
+        {
+            throw new NotImplementedException();
+        }
+        
     }
 }
