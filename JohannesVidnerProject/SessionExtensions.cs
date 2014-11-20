8631d40b1e1466f998587f48a6e011323a8cb654
Kasper Guldmann Nielsen <kalleguld@gmail.com>
2014-11-20 14:14:13 +0100

    Removed SessionService
    Use Session.GetCurrentUser() instead (works only inside Controllers)

---
+++ JohannesVidnerProject/SessionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+using Microsoft.Ajax.Utilities;
+using Model;
+using Services;
+
+namespace JohannesVidnerProject
+{
+
+    public static class SessionExtensions
+    {
+        public static User GetCurrentUser(this HttpSessionStateBase baseSession)
+        {
+            return baseSession["CurrentUser"] as User;
+        }
+
+        public static void SetCurrentUser(this HttpSessionStateBase baseSession, User user)
+        {
+            baseSession["CurrentUser"] = user;
+        }
+
+
+    }
+}
