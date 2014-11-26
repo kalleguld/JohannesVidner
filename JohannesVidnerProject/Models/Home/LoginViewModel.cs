321908cca224a63c46341537b5b9398c8f0d0a46
B0773N <robertrholm@gmail.com>
2014-11-26 13:30:30 +0100

    Dansk oversættelse

--- JohannesVidnerProject/Models/Home/LoginViewModel.cs
+++ JohannesVidnerProject/Models/Home/LoginViewModel.cs
@@ -1,18 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Resources;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+using Newtonsoft.Json;
+using Resources;
 
 namespace JohannesVidnerProject.Models.Home
 {
-    
+    //HttpContext.GetGlobalResourceObject(“Resourcefilename”, “resourcekey″).ToString();
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Username")]
-        
         public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
         public string Password { get; set; }
     }
 }
