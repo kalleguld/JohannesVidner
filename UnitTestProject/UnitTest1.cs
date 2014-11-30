91325e4dbbde9bc5186ea415d4acdb5577646797
Kasper Guldmann Nielsen <git@kalleguld.dk>
2014-11-30 02:08:32 +0100

    Refactored unit tests

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -1,9 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using JohannesVidnerProject;
 using JohannesVidnerProject.Controllers;
 using JohannesVidnerProject.Models.Home;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Model;
+using Moq;
 using Services;
 
 namespace UnitTestProject
@@ -11,20 +14,29 @@ namespace UnitTestProject
     [TestClass]
     public class UnitTest1
     {
+
+        [TestMethod]
+        public void TestObscureSearch()
+        {
+            var inViewModel = new IndexViewModel{q = "sdhfsjfsvkjbsdgfsuh"};
+            var user = DbService.Instance.GetUserById(1);
+
+            var outViewModel = StartHomeController(inViewModel, user);
+            Assert.AreEqual(0, outViewModel.PublicationViewModels.Count);
+        }
+
+        
+
+
         //test of a search by only one word, with top administrator level.
         [TestMethod]
         public void TestSearchTopLevelOneWord()
         {
-            var hc = new HomeController();
-            var hcPo = new PrivateObject(hc);
-            var ivModel = new IndexViewModel();
             var user = DbService.Instance.GetUserById(1);
+            var ivModel = new IndexViewModel {q = "ipsum"};
 
-            ivModel.q = "ipsum";
+            var outVm = StartHomeController(ivModel, user);
 
-            var outVm = (IndexViewModel) hcPo.Invoke("FillViewModel", 
-                                                     ivModel, 
-                                                     user.Publication);
             Assert.AreEqual(6, outVm.PublicationViewModels.Count);
         }
 
@@ -32,15 +44,11 @@ namespace UnitTestProject
         [TestMethod]
         public void TestSearchTopLevelManyWords()
         {
-            var hc = new HomeController();
-            var hcPo = new PrivateObject(hc);
-            var ivModel = new IndexViewModel();
             var user = DbService.Instance.GetUserById(1);
+            var ivModel = new IndexViewModel {q = "ipsum llc"};
 
-            ivModel.q = "ipsum llc";
-            var outVm = (IndexViewModel)hcPo.Invoke("FillViewModel",
-                                                     ivModel,
-                                                     user.Publication);
+            var outVm = StartHomeController(ivModel, user);
+
             Assert.AreEqual(2, outVm.PublicationViewModels.Count);
         }
 
@@ -48,22 +56,51 @@ namespace UnitTestProject
         [TestMethod]
         public void TestSearchLowerLevel()
         {
-            var hc = new HomeController();
-            var hcPo = new PrivateObject(hc);
-            var ivModel = new IndexViewModel();
             var user = DbService.Instance.GetUserById(1);
+            var ivModel = new IndexViewModel
+            {
+                p = 3, 
+                q = "in"
+            };
 
-            ivModel.p = 3;
-            ivModel.q = "in";
-            
-            var outVm = (IndexViewModel)hcPo.Invoke("FillViewModel",
-                                                     ivModel,
-                                                     user.Publication);
+            var outVm = StartHomeController(ivModel, user);
+
             Assert.AreEqual(1, outVm.PublicationViewModels.Count);
         }
 
 
+        private static IndexViewModel StartHomeController(IndexViewModel inViewModel, User user)
+        {
+            var controller = new HomeController();
+            var mockSession = new MockSession();
+            SetFakeSession(controller, mockSession);
 
+            mockSession.SetCurrentUser(user);
 
+            var viewResult = (ViewResult)controller.Index(inViewModel);
+            var outViewModel = (IndexViewModel)viewResult.Model;
+            return outViewModel;
+        }
+
+        private static void SetFakeSession(ControllerBase controller, HttpSessionStateBase mockSession)
+        {
+            //The Mock class is part of Moq, found on NuGet
+            var mockHttpContextBase = new Mock<HttpContextBase>();
+            mockHttpContextBase.SetupGet(hcb => hcb.Session).Returns(mockSession);
+            var mockControllerContext = new Mock<ControllerContext>();
+            mockControllerContext.SetupGet(hcc => hcc.HttpContext).Returns(mockHttpContextBase.Object);
+            controller.ControllerContext = mockControllerContext.Object;
+        }
+
+    }
+
+    class MockSession : HttpSessionStateBase
+    {
+        readonly Dictionary<string, object> _dict = new Dictionary<string, object>(); 
+        override public object this[string key]
+        {
+            get { return _dict[key]; }
+            set { _dict[key] = value; }
+        }
     }
 }
